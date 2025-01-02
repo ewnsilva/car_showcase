9e4a154e76d03bbfbfad48540d812f615c210deb
Ericles Willian <ewnsilva@gmail.com>
2025-01-02 19:32:51 -0300

    feat: adding car card

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Hero, SearchBar, CostumFilter } from "../components";
+import { Hero, SearchBar, CostumFilter, CarCard } from "../components";
 import { fetchCars } from "../utils";
 
 export default async function Home() {
   const allCars = await fetchCars();
 
-  console.log(allCars);
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -22,6 +22,21 @@ export default async function Home() {
             <CostumFilter title="fuel" />
             <CostumFilter title="year" />
           </div>
+
+          {!isDataEmpty ? (
+            <section>
+              <div className="home__cars-wrapper">
+                {allCars?.map((car) => (
+                  <CarCard car={car} key={car.model} />
+                ))}
+              </div>
+            </section>
+          ) : (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )}
         </div>
       </div>
     </main>
