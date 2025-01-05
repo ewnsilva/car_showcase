1e82d60139e2a2589cd75631efc9dae083ebf082
Ericles Willian <ewnsilva@gmail.com>
2025-01-05 17:55:05 -0300

    feat: showing car deflectors when clicking on view more button

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,11 +3,12 @@
 import Image from "next/image";
 
 import { CustomButton } from "./CustomButton";
-import { CarCardProps } from "../types";
+import { CarProps } from "../types";
 import { calculateCarRent } from "../utils";
 import { useState } from "react";
+import { CarDetails } from "./CarDetails";
 
-export const CarCard: React.FC<{ car: CarCardProps }> = ({ car }) => {
+export const CarCard: React.FC<{ car: CarProps }> = ({ car }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const { city_mpg, drive, make, model, mpg, transmission, year } = car;
@@ -18,7 +19,6 @@ export const CarCard: React.FC<{ car: CarCardProps }> = ({ car }) => {
     <div className="car-card group">
       <div className="car-card__content">
         <h2 className="car-card__content-title">
-          {" "}
           {make} {model}
         </h2>
       </div>
@@ -29,7 +29,7 @@ export const CarCard: React.FC<{ car: CarCardProps }> = ({ car }) => {
         <span className="self-end text=[14px] font-semibold">/day</span>
       </p>
 
-      <div className="relative w-full h-40 my-3 object contain">
+      <div className="relative w-full h-40 my-3 object-contain">
         <Image
           src="/hero.png"
           alt="car model"
@@ -39,11 +39,11 @@ export const CarCard: React.FC<{ car: CarCardProps }> = ({ car }) => {
         />
       </div>
 
-      <div className="relative flex w-full mt2">
+      <div className="relative flex w-full mt-2">
         <div className="flex group-hover:invisible w-full justify-between text-gray">
           <div className="flex flex-col justify-center items-center gap-2">
             <Image
-              src="/steering-whell.svg"
+              src="/steering-wheel.svg"
               width={20}
               height={20}
               alt="sterring wheel"
@@ -60,19 +60,24 @@ export const CarCard: React.FC<{ car: CarCardProps }> = ({ car }) => {
             <Image src="/gas.svg" width={20} height={20} alt="gas" />
             <p className="text=[14px]">{city_mpg} MPG</p>
           </div>
+        </div>
 
-          <div className="car-card__btn-container">
-            <CustomButton
-              title="View More"
-              containerStyles="w-full py=[16px]
-            rounded-full bg-primary-blue"
-              textStyles="text-white text-[14px] leading-[17px] font-bold"
-              rightIcon="/right-arrow.svg"
-              handleClick={() => setIsOpen(true)}
-            />
-          </div>
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
         </div>
       </div>
+
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
