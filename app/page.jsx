47414295c526818d0182e8ef13ad5563cc2bc38b
Ericles Willian <ewnsilva@gmail.com>
2025-01-05 17:51:42 -0300

    test: fixing error on file type

--- app/page.jsx
+++ app/page.jsx
@@ -22,22 +22,21 @@ export default async function Home() {
             <CostumFilter title="fuel" />
             <CostumFilter title="year" />
           </div>
-
-          {!isDataEmpty ? (
-            <section>
-              <div className="home__cars-wrapper">
-                {allCars?.map((car) => (
-                  <CarCard car={car} key={} />
-                ))}
-              </div>
-            </section>
-          ) : (
-            <div className="home__error-container">
-              <h2 className="text-black text-xl font-bold">Oops, no results</h2>
-              <p>{allCars?.message}</p>
-            </div>
-          )}
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car, index) => (
+                <CarCard car={car} key={index} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
