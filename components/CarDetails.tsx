21551f47491cf7cdc90ef1b054e162334aa362cb
Ericles Willian <ewnsilva@gmail.com>
2025-01-07 20:45:54 -0300

    refactor: using endpoint to search car image

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -10,6 +10,7 @@ import {
   TransitionChild,
 } from "@headlessui/react";
 import { Fragment } from "react";
+import { generateCarImageUrl } from "../utils";
 
 interface CarDetailsProps {
   isOpen: boolean;
@@ -65,7 +66,7 @@ export const CarDetails: React.FC<CarDetailsProps> = ({
                   <div className="flex-1 flex flex-col gap-3">
                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                       <Image
-                        src="/hero.png"
+                        src={generateCarImageUrl(car)}
                         alt="car model"
                         fill
                         priority
@@ -76,7 +77,7 @@ export const CarDetails: React.FC<CarDetailsProps> = ({
                     <div className="flex gap-3">
                       <div className="flax-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, "29")}
                           alt="car model"
                           fill
                           priority
@@ -85,7 +86,7 @@ export const CarDetails: React.FC<CarDetailsProps> = ({
                       </div>
                       <div className="flax-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, "33")}
                           alt="car model"
                           fill
                           priority
@@ -94,7 +95,7 @@ export const CarDetails: React.FC<CarDetailsProps> = ({
                       </div>
                       <div className="flax-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src="/hero.png"
+                          src={generateCarImageUrl(car, "13")}
                           alt="car model"
                           fill
                           priority
