4b1aa345b98d0bd80b9992c465c1bef482d17489
Ericles Willian <ewnsilva@gmail.com>
2025-01-07 21:54:43 -0300

    refactor: search button component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,19 @@
 
 import { useState } from "react";
 import { SearchManufacturer } from "./SearchManufacturer";
+import Image from "next/image";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="button" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 export const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
@@ -13,6 +26,7 @@ export const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
     </form>
   );
