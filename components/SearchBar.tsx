9152486898b9dda0ab229559f6b81f160af1f903
Ericles Willian <ewnsilva@gmail.com>
2025-01-11 09:33:19 -0300

    feat: creating search parameters

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { useState } from "react";
-import { SearchManufacturer } from "./SearchManufacturer";
+
+import { useRouter } from "next/navigation";
 import Image from "next/image";
+
+import { SearchManufacturer } from "./SearchManufacturer";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="button" className={`-ml-3 z-10 ${otherClasses}`}>
@@ -17,10 +20,42 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 );
 
 export const SearchBar = () => {
+  const router = useRouter();
   const [manufacturer, setManufacturer] = useState("");
 
   const [model, setModel] = useState("");
-  const handleSearch = () => {};
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in the search bar");
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    const newPathname = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
+
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
