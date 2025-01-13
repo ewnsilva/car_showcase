cf5173f03b9af89d6d600f413128fc33fafdc812
Ericles Willian <ewnsilva@gmail.com>
2025-01-12 23:08:00 -0300

    feat: custom filter for searching vehicles info

--- components/CostumFilter.tsx
+++ components/CostumFilter.tsx
@@ -1,8 +1,48 @@
 "use client";
-interface CostumFilterProps {
-  title: string;
-}
 
-export const CostumFilter: React.FC<CostumFilterProps> = ({ title }) => {
-  return <div>CostumFilter</div>;
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { Listbox, ListboxButton, Transition } from "@headlessui/react";
+import { CustomFilterProps } from "../types";
+import { updateSearchParams } from "../utils";
+
+export const CostumFilter: React.FC<CustomFilterProps> = ({
+  title,
+  options,
+}) => {
+  const router = useRouter();
+  const [selected, setSelected] = useState(options[0]);
+
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName);
+  };
+
+  return (
+    <div className="w-fit">
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
+      >
+        <div className="relative w-fit z-10">
+          <ListboxButton className="custom-bilter__btn">
+            <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+              alt="chevron up down"
+            />
+          </ListboxButton>
+        </div>
+      </Listbox>
+    </div>
+  );
 };
