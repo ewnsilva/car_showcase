fb028f7db6136ff6bfea5b3cf7241cc9f9367779
Ericles Willian <ewnsilva@gmail.com>
2025-01-13 23:43:56 -0300

    refactor: creating show more component

---
+++ components/ShowMore.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { ShowMoreProps } from "../types";
+
+export const ShowMore: React.FC<ShowMoreProps> = ({ isNext, pageNumber }) => {
+  return <div>ShowMore</div>;
+};
