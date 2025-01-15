2919227c7942f1dd1467e8912675906bb9593918
Ericles Willian <ewnsilva@gmail.com>
2025-01-14 21:59:30 -0300

    feat: show more cars funtion

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -3,11 +3,17 @@
 import { useRouter } from "next/navigation";
 import { ShowMoreProps } from "../types";
 import { CustomButton } from "./CustomButton";
+import { updateSearchParams } from "../utils";
 
 export const ShowMore: React.FC<ShowMoreProps> = ({ isNext, pageNumber }) => {
   const router = useRouter();
 
-  const handleNavigation = () => {};
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathName);
+  };
 
   return (
     <div className="w-full flex-center gap-5 mt-10">
