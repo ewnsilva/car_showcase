b9557286af139f9a621e7212e36e47dbd1c0f930
Ericles Willian <ewnsilva@gmail.com>
2025-01-28 21:10:24 -0300

    refactor: minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Footer, NavBar } from "../components";
+
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -9,9 +10,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className="relative">
